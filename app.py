cd5b919e2ea9d715d3d1c6f00c067cc7da3bb7b3
Ethan Wen <wenethan6@gmail.com>
2023-06-24 22:42:03 +0800

    Add a chatbot

--- app.py
+++ app.py
@@ -10,6 +10,11 @@ app = Flask(__name__, static_folder='static')
 def index():
     return render_template('index.html')
 
+@app.route('/chatbot')
+def chatbot():
+    return render_template('chatbot.html')
+
+
 @app.route('/generate-diagram', methods=['POST'])
 def generate_diagram():
     plantuml_code = request.json['code']
@@ -24,6 +29,8 @@ def generate_diagram():
     return jsonify({'image_base64': image_base64})
 
 openai.api_key = "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"
+
+
 
 @app.route('/process-user-input', methods=['POST'])
 def process_user_input():
@@ -51,11 +58,38 @@ def process_user_input():
     start_index = reply_content.index(start_delimiter)
     end_index = reply_content.index(end_delimiter) + len(end_delimiter)
 
+    selected_theme = request.json['theme']
     code_block = reply_content[start_index:end_index]
-    code_block = "!theme minty\n" + code_block
+    code_block = "!theme " + selected_theme + "\n" + code_block
 
     print(code_block)
     return jsonify({'reply': code_block})
+
+# chatbot页面功能
+# 聊天路由
+@app.route('/send_message', methods=['POST'])
+def process_message():
+    message = request.json['message']
+    reply = send_to_chatgpt(message)  # 调用后端代码生成回复
+    return reply
+
+def send_to_chatgpt(message):
+    # 构造对话历史，替换示例助手的部分
+    messages = [
+        {"role": "system", "content": "你是一个联易融国际供应链金融方向的智能客服"},
+        {"role": "system", "content": "联易融国际的主要产品有Digital Trade，Digital Finance 和 Digital Payment"},
+        {"role": "system", "name": "example_user", "content": message}
+    ]
+
+    # 调用OpenAI的ChatGPT模型生成回复
+    response = openai.ChatCompletion.create(
+        model="gpt-3.5-turbo",
+        messages=messages,
+        temperature=0.7
+    )
+    reply_content = response["choices"][0]["message"]["content"]
+    print(reply_content)
+    return jsonify({'reply': reply_content})
 
 
 if __name__ == '__main__':
